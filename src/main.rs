a4b856a379cfe1ad168697fb5974009c7c702785
Christman-Gavin <christman.gavin.r@gmail.com>
2025-04-27 09:40:36 -0500

    feat/cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::time::SystemTime;
-
 // Rotor    ABCDEFGHIJKLMNOPQRSTUVWXYZ 	    Date-Introduced 	Model Name & Number
 
 // I 	    EKMFLGDQVZNTOWYHXUSPAIBRCJ 	    1930 	            Enigma I
